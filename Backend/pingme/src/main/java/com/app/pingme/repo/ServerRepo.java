6747ebe8d41cc941abdb85cc76936e4b70c2ce8d
Ravi Prakash <ravimehra1264@gmail.com>
2023-03-31 22:59:09 +0530

    final commit

---
+++ Backend/pingme/src/main/java/com/app/pingme/repo/ServerRepo.java
@@ -0,0 +1,8 @@
+package com.app.pingme.repo;
+
+import com.app.pingme.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepo extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
